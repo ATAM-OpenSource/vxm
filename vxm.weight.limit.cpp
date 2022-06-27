bc99e0787a21466dc5789e2f3bf09555835806f4
EnesYildirim1 <108119567+EnesYildirim1@users.noreply.github.com>
2022-06-27 09:09:20 +0300

    Update vxm.weight.limit.cpp

--- vxm.weight.limit.cpp
+++ vxm.weight.limit.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "param.h"
 using namespace std;
-int main()
+int main(int argc, char *argv[])
 {
+		string help = "This program shows the weight limits for rotary table. The working example is a follows:\n\
+.\vxm.weight.limit.exe ";
+	params(argc, argv, help);
 	cout << "The vertical weight limit is 11.34 kilograms.\n";
 	cout << "The horizonal weight limit is 90.71 kilograms.\n";
 
