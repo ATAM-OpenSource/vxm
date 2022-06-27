1d10348f9dff686ad2734dd0a1dfe1e307ed2688
artun-akdogan <artun.akdogan@boun.edu.tr>
2022-06-27 09:12:33 +0300

    Add help for vxm.speed.limit

--- vxm.speed.limit.cpp
+++ vxm.speed.limit.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-{ 
 	string help = "This program shows the safe ranges of speed.";
 	params(argc, argv, help);
 	cout << "Upper Speed Limit: 33.33\nLower Speed Limit: 4.00\nPlease note that those are recommended values\n";
