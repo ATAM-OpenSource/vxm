28a96d5516ac4b69dcd759b1ffe743c466a9a4ab
artun-akdogan <artun.akdogan@boun.edu.tr>
2022-06-27 15:18:59 +0300

    Add auto port detection

--- commons.h
+++ commons.h
@@ -1,9 +1,24 @@
 #ifndef COMMONS_H
 #define COMMONS_H
 
+#include <iostream>
+
+using namespace std;
+
 void init(){
 	LoadDriver("VxmDriver.dll");
-	PortOpen(3, 9600);
+	int port = 0;
+	for(int i=1; i<=4; i++){
+		if(PortOpen(i, 9600)){
+			port = i;
+			break;
+		}
+	}
+	if(port==0){
+		cout << "Device not found. Please ensure that BPS is 9600." << endl;
+		exit(0);
+	}
+	
 	PortClear();
     PortSendCommands("F,C,R");
 }
