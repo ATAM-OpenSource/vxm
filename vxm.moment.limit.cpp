490beb44b7d2c29ac1304d16e87f70be2f229b0f
EnesYildirim1 <108119567+EnesYildirim1@users.noreply.github.com>
2022-06-27 09:18:05 +0300

    Update vxm.moment.limit.cpp

--- vxm.moment.limit.cpp
+++ vxm.moment.limit.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	string help = "This program shows the moment limits of rotary table.The working example is a follows:\n\
-.\vxm.moment.limit.exe";
+.\\vxm.moment.limit.exe";
 	params(argc, argv, help);
 	cout << "The moment limit is 56.49 Newton Meters.\n";
 	return 0;
